c548c3dba3cf81458cf56f41c4759a259a38d840
unknown <x15g006@PC504N011.chiba-fjb.ac.jp>
2015-12-14 14:38:28 +0900

    課題12/14②

--- src/jp/ac/chibafjb/x15g006/kd172/Main50.java
+++ src/jp/ac/chibafjb/x15g006/kd172/Main50.java
@@ -3,10 +3,17 @@ package jp.ac.chibafjb.x15g006.kd172;
 public class Main50 {
 
 	public static void main(String[] args) {
-		int m = 3;
-		int n = 2;
 		int[] x={1,2,3};
 		int[] y={7,8};
+		int[] z= new int [x.length+y.length];
+		System.out.print("z[]={");
+		for(int i=0;i<x.length;i++)
+			z[i] += x[i];
+		for(int o=0+x.length;o<y.length+x.length;o++)
+			z[o] += y[o-x.length];
+		for(int p=0;p<x.length+y.length;p++)
+			System.out.print(z[p]+",");
+		System.out.print("}");
 
 	}
 
